ddd540b6c349473bd8155b5e1e3c49d5a15de9d5
truongvinh97 <70201168+truongvinh97@users.noreply.github.com>
2023-05-14 16:19:23 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-import RPi.GPIO as GPIO
-import time
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+#!/bin/python3
 
-# Set up Firebase Realtime Database credentials
-cred = credentials.Certificate('rpi-control.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://rpi-control-4fc05.firebaseapp.com/'
-})
+import RPi.GPIO as GPIO
+import pyrebase
+import time
 
 # Set up GPIO pins for LIGHT1 and LIGHT2
 GPIO.setmode(GPIO.BCM)
@@ -16,45 +10,49 @@ GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 
-# Define a function to toggle the value of LIGHT1 and update the database
+# Set up Pyrebase config and database
+config = {
+    "apiKey": "YOUR_API_KEY",
+    "authDomain": "YOUR_PROJECT_ID.firebaseapp.com",
+    "databaseURL": "https://YOUR_PROJECT_ID.firebaseio.com",
+    "storageBucket": "YOUR_PROJECT_ID.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+# Define a function to toggle the value of LIGHT1
 def light1Toggle():
     # Get the current value of LIGHT1 from the database
-    currentLight1Value = db.reference('/node1/light1').get() or False
+    currentLight1Value = db.child("node1").child("light1").get().val()
 
     # Toggle the value of LIGHT1 and update the database
     newLight1Value = not currentLight1Value
-    db.reference('/node1/light1').set(newLight1Value)
+    db.child("node1").update({"light1": newLight1Value})
 
     # Update the state of LIGHT1
     GPIO.output(17, newLight1Value)
 
-# Define a function to toggle the value of LIGHT2 and update the database
+# Define a function to toggle the value of LIGHT2
 def light2Toggle():
     # Get the current value of LIGHT2 from the database
-    currentLight2Value = db.reference('/node1/light2').get() or False
+    currentLight2Value = db.child("node1").child("light2").get().val()
 
     # Toggle the value of LIGHT2 and update the database
     newLight2Value = not currentLight2Value
-    db.reference('/node1/light2').set(newLight2Value)
+    db.child("node1").update({"light2": newLight2Value})
 
     # Update the state of LIGHT2
     GPIO.output(27, newLight2Value)
 
-# Set up a listener for the "Action" field in node2
-actionRef = db.reference('/node2/Action')
-def actionListener(event):
-    # Get the value of the "Action" field
-    action = event.data
-
-    # Call the appropriate function based on the action
-    if action == 'light1Toggle':
-        light1Toggle()
-    elif action == 'light2Toggle':
-        light2Toggle()
-
-# Attach the listener to the "Action" field
-actionRef.listen(actionListener)
-
 # Wait for events
 while True:
+    # Check for changes in LIGHT1 and LIGHT2 and update the states accordingly
+    light1State = db.child("node1").child("light1").get().val()
+    GPIO.output(17, light1State)
+
+    light2State = db.child("node1").child("light2").get().val()
+    GPIO.output(27, light2State)
+
+    # Wait for 1 second before checking again
     time.sleep(1)
